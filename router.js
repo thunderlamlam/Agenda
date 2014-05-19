a49003ef947fce9f0d9bd52d7a1650ac86facb5c
Emily Lam <lam.h.emily@gmail.com>
2014-05-19 11:06:32 -0700

    Update router.js

--- router.js
+++ router.js
@@ -3,12 +3,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-	this.resource('todos', { path: '/'}, function () {
-		//additional child routes
-		this.route('active');
-		this.route('completed');
-		
-	});
+  this.resource('todos', { path: '/'}, function () {
+    //additional child routes
+    this.route('active');
+    this.route('completed');
+  });
 });
 
 export default Router;
